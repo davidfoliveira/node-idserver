4d96ec0363c20f5b953ec597edfec1759436a14a
David Oliveira <d.oliveira@prozone.org>
2014-10-26 01:40:21 +0100

    Cleanup

--- bin/server.js
+++ bin/server.js
@@ -9,16 +9,4 @@ var
 // Defaults to port 1970
 server.start(function(){
 
-/*    setInterval(function(){
-        server.bizAskIDs('x',10,function(err,ids){
-            if ( err ) {
-                console.log("Can't get id's: ",err);
-                return process.exit(-1);
-            }
-
-            console.log("ids: ",ids);
-        });
-    },0);
-*/
-
 });
